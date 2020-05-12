1bb895d290349a88bf0e64efe7a20354753f1ccc
Jean M. Lescure <jeanmlescure@gmail.com>
2020-05-12 15:31:15 -0600

    release v3

---
+++ runkit.js
@@ -0,0 +1,10 @@
+const {default: ShortUniqueId} = require('short-unique-id');
+
+console.log('ShortUniqueId', ShortUniqueId);
+
+const uid = new ShortUniqueId();
+
+console.log(uid(6));
+console.log(uid.seq());
+
+console.log(uid.version);
--- test/runkit.js
+++
@@ -1,5 +0,0 @@
-const ShortUniqueId = require('short-unique-id').default;
-
-const uid = new ShortUniqueId();
-
-console.log(uid.randomUUID(6));
